238546beb78bf9d4ba425bd01f002041caf74e46
Ram Shinde <5203767+ram-ks@users.noreply.github.com>
2026-04-07 17:11:26 +0530

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root) {
+        const result = this.dfs(root);
+        return result !== -1;
+    }
+
+    dfs(root) {
+        if (!root) {
+            return 0;
+        }
+
+        const left = this.dfs(root.left);
+        const right = this.dfs(root.right);
+
+        if (left === -1 || right === -1) {
+            return -1;
+        }
+
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(left, right);
+    }
+}
